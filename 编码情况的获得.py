7b2b5601b901df06ed61e2cb7423e4d33d34a6cd
shydoors <15031719+shydoors@user.noreply.gitee.com>
2024-11-17 01:08:11 +0800

    emm,帮人测试的一个代码

---
+++ 编码情况的获得.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import chardet
+import os
+def read_files_in_folder(folder_path):
+    coding_s = {}
+    for filename in os.listdir(folder_path):
+        file_path = os.path.join(folder_path, filename)
+        if os.path.isfile(file_path):
+            with open(file_path, 'rb') as f:
+                encoding = chardet.detect(f.read())['encoding']
+                coding_s.setdefault(encoding, 0)
+                coding_s[encoding] += 1
+    return coding_s
+
+# 示例使用
+folder_path = "Appendix-3"
+s = read_files_in_folder(folder_path)
+df=pd.DataFrame(list(s.items()), columns=['encoding', 'count'], index=None)
+print(df.sort_values(by='count', ascending=True).to_string(index=False))
+# ascending=True时根据编码方式的出现次数升序输出
